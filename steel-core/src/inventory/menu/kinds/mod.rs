26c8dd059bf8c1e5ab425bca963e40c1209771aa
Ezra Clintoc <140189732+ezraclintoc@users.noreply.github.com>
2026-09-22 10:10:33 -0500

    Feat/grindstone (#509)
    Co-authored-by: coco875 <59367621+coco875@users.noreply.github.com>
    Co-authored-by: CuzImClicks <cuzimclicks@proton.me>

--- steel-core/src/inventory/menu/kinds/mod.rs
+++ steel-core/src/inventory/menu/kinds/mod.rs
@@ -7,6 +7,7 @@ mod chest_menu;
 mod crafting_menu;
 mod ender_chest_menu;
 mod furnace_menu;
+mod grindstone_menu;
 mod inventory_menu;
 mod shulker_box_menu;
 
@@ -17,5 +18,6 @@ pub use chest_menu::{ChestKind, chest};
 pub use crafting_menu::{CraftingKind, crafting};
 pub use ender_chest_menu::{EnderChestKind, ender_chest};
 pub use furnace_menu::{FurnaceMenuKind, furnace_menu};
+pub use grindstone_menu::{GrindstoneKind, grindstone};
 pub use inventory_menu::{INVENTORY_MENU_CONTAINER_ID, InventoryKind, inventory_menu};
 pub use shulker_box_menu::{ShulkerBoxKind, shulker_box};
